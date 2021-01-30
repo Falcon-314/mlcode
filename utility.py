8ce123aa6a2e474122710fd48317467bd122d60b
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-01-30 17:44:26 +0900

    Create utility.py

---
+++ utility.py
@@ -0,0 +1,26 @@
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    LOGGER.info(f'[{name}] start')
+    yield
+    LOGGER.info(f'[{name}] done in {time.time() - t0:.0f} s.')
+
+def init_logger(log_file=OUTPUT_DIR+'train.log'):
+    from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=log_file)
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+def seed_torch(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
