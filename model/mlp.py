9213d82dd01a5df2ca5c83eb8abcf0b8d270edf4
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-02-05 22:27:50 +0900

    Create mlp.py

---
+++ model/mlp.py
@@ -0,0 +1,388 @@
+#Dataset
+class MLPDataset_train:
+    def __init__(self, features, targets):
+        self.features = features
+        self.targets = targets
+        
+    def __len__(self):
+        return (self.features.shape[0])
+    
+    def __getitem__(self, idx):
+        dct = {
+            'x' : torch.tensor(self.features[idx, :], dtype=torch.float),
+            'y' : torch.tensor(self.targets[idx, ], dtype=torch.long) #targetのsizeにあわせて変更  
+        }
+        return dct
+    
+class MLPDataset_test:
+    def __init__(self, features):
+        self.features = features
+        
+    def __len__(self):
+        return (self.features.shape[0])
+    
+    def __getitem__(self, idx):
+        dct = {
+            'x' : torch.tensor(self.features[idx, :], dtype=torch.float)
+        }
+        return dct
+#model
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.dense1 = nn.Linear(15, 12)
+        
+        self.dropout2 = nn.Dropout(0.2)
+        self.dense2 = nn.Linear(12,12)
+        
+        self.dropout3 = nn.Dropout(0.2)
+        self.dense3 = nn.Linear(12, 5)
+    
+    def forward(self, x):
+        
+        x = self.dense1(x)
+        x = F.relu(x)
+        
+        x = self.dropout2(x)
+        x = self.dense2(x)
+        x = F.relu(x)
+        
+        x = self.dropout3(x)
+        x = self.dense3(x)
+        
+        return x
+#loss
+class LabelSmoothingLoss(nn.Module): 
+    def __init__(self, classes=5, smoothing=0.0, dim=-1): 
+        super(LabelSmoothingLoss, self).__init__() 
+        self.confidence = 1.0 - smoothing 
+        self.smoothing = smoothing 
+        self.cls = classes 
+        self.dim = dim 
+    def forward(self, pred, target): 
+        pred = pred.log_softmax(dim=self.dim) 
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred) 
+            true_dist.fill_(self.smoothing / (self.cls - 1)) 
+            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence) 
+        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))    
+    
+    
+class TaylorSoftmax(nn.Module):
+    '''
+    This is the autograd version
+    '''
+    def __init__(self, dim=1, n=2):
+        super(TaylorSoftmax, self).__init__()
+        assert n % 2 == 0
+        self.dim = dim
+        self.n = n
+
+    def forward(self, x):
+        '''
+        usage similar to nn.Softmax:
+            >>> mod = TaylorSoftmax(dim=1, n=4)
+            >>> inten = torch.randn(1, 32, 64, 64)
+            >>> out = mod(inten)
+        '''
+        fn = torch.ones_like(x)
+        denor = 1.
+        for i in range(1, self.n+1):
+            denor *= i
+            fn = fn + x.pow(i) / denor
+        out = fn / fn.sum(dim=self.dim, keepdims=True)
+        return out
+
+    
+class TaylorCrossEntropyLoss(nn.Module):
+    def __init__(self, n=2, ignore_index=-1, reduction='mean', smoothing=0.05):
+        super(TaylorCrossEntropyLoss, self).__init__()
+        assert n % 2 == 0
+        self.taylor_softmax = TaylorSoftmax(dim=1, n=n)
+        self.reduction = reduction
+        self.ignore_index = ignore_index
+        self.lab_smooth = LabelSmoothingLoss(CFG.target_size, smoothing=smoothing)
+
+    def forward(self, logits, labels):
+        log_probs = self.taylor_softmax(logits).log()
+        #loss = F.nll_loss(log_probs, labels, reduction=self.reduction,
+        #        ignore_index=self.ignore_index)
+        loss = self.lab_smooth(log_probs, labels)
+        return loss
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (remain %s)' % (asMinutes(s), asMinutes(rs))
+def train_fn(train_loader, model, criterion, optimizer, epoch, scheduler, device):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    scores = AverageMeter()
+    # switch to train mode
+    model.train()
+    start = end = time.time()
+    global_step = 0
+    for step, data in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        inputs, targets = data['x'].to(device), data['y'].to(device)
+        batch_size = CFG.batch_size
+        y_preds = model(inputs)
+        loss = criterion(y_preds, targets)
+        # record loss
+        losses.update(loss.item(), batch_size)
+        if CFG.gradient_accumulation_steps > 1:
+            loss = loss / CFG.gradient_accumulation_steps
+        if CFG.apex:
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), CFG.max_grad_norm)
+        grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), CFG.max_grad_norm)
+        if (step + 1) % CFG.gradient_accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            global_step += 1
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if step % CFG.print_freq == 0 or step == (len(train_loader)-1):
+            print('Epoch: [{0}][{1}/{2}] '
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f}) '
+                  'Elapsed {remain:s} '
+                  'Loss: {loss.val:.4f}({loss.avg:.4f}) '
+                  'Grad: {grad_norm:.4f}  '
+                  #'LR: {lr:.6f}  '
+                  .format(
+                   epoch+1, step, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses,
+                   remain=timeSince(start, float(step+1)/len(train_loader)),
+                   grad_norm=grad_norm,
+                   #lr=scheduler.get_lr()[0],
+                   ))
+    return losses.avg
+
+
+def valid_fn(valid_loader, model, criterion, device):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    scores = AverageMeter()
+    # switch to evaluation mode
+    model.eval()
+    preds = []
+    start = end = time.time()
+    
+    for step, data in enumerate(valid_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        inputs, targets = data['x'].to(device), data['y'].to(device)
+        batch_size = CFG.batch_size
+        # compute loss
+        with torch.no_grad():
+            y_preds = model(inputs)
+        loss = criterion(y_preds, targets)
+        losses.update(loss.item(), batch_size)
+        # record accuracy
+        preds.append(y_preds.softmax(1).to('cpu').numpy())
+        if CFG.gradient_accumulation_steps > 1:
+            loss = loss / CFG.gradient_accumulation_steps
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if step % CFG.print_freq == 0 or step == (len(valid_loader)-1):
+            print('EVAL: [{0}/{1}] '
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f}) '
+                  'Elapsed {remain:s} '
+                  'Loss: {loss.val:.4f}({loss.avg:.4f}) '
+                  .format(
+                   step, len(valid_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses,
+                   remain=timeSince(start, float(step+1)/len(valid_loader)),
+                   ))
+    predictions = np.concatenate(preds)
+    return losses.avg, predictions
+
+
+def inference(model, states, test_loader, device):
+    model.to(device)
+    tk0 = tqdm(enumerate(test_loader), total=len(test_loader))
+    probs = []
+    for i, data in tk0:
+        inputs = data['x'].to(device)
+        avg_preds = []
+        for state in states:
+            model.load_state_dict(state['model'])
+            model.eval()
+            with torch.no_grad():
+                y_preds = model(inputs)
+            avg_preds.append(y_preds.softmax(1).to('cpu').numpy())
+        avg_preds = np.mean(avg_preds, axis=0)
+        probs.append(avg_preds)
+    probs = np.concatenate(probs)
+    return probs
+def train_loop(folds, fold, features):
+
+    LOGGER.info(f"========== fold: {fold} training ==========")
+
+    # ====================================================
+    # loader
+    # ====================================================
+    trn_idx = folds[folds['fold'] != fold].index
+    val_idx = folds[folds['fold'] == fold].index
+
+    train_folds = folds.loc[trn_idx].reset_index(drop=True)
+    valid_folds = folds.loc[val_idx].reset_index(drop=True)
+    
+    train_dataset = MLPDataset_train(train_folds[features].values,train_folds['label'].values)
+    valid_dataset = MLPDataset_train(valid_folds[features].values,valid_folds['label'].values)
+
+    train_loader = DataLoader(train_dataset, 
+                              batch_size=CFG.batch_size, 
+                              shuffle=True, 
+                              num_workers=CFG.num_workers, pin_memory=True, drop_last=True)
+    valid_loader = DataLoader(valid_dataset, 
+                              batch_size=CFG.batch_size, 
+                              shuffle=False, 
+                              num_workers=CFG.num_workers, pin_memory=True, drop_last=False)
+    
+    # ====================================================
+    # scheduler 
+    # ====================================================
+    def get_scheduler(optimizer):
+        if CFG.scheduler=='ReduceLROnPlateau':
+            scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=CFG.factor, patience=CFG.patience, verbose=True, eps=CFG.eps)
+        elif CFG.scheduler=='CosineAnnealingLR':
+            scheduler = CosineAnnealingLR(optimizer, T_max=CFG.T_max, eta_min=CFG.min_lr, last_epoch=-1)
+        elif CFG.scheduler=='CosineAnnealingWarmRestarts':
+            scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=CFG.T_0, T_mult=1, eta_min=CFG.min_lr, last_epoch=-1)
+        return scheduler
+
+    # ====================================================
+    # model & optimizer
+    # ====================================================
+    model = MLP()
+    model.to(device)
+
+    optimizer = Adam(model.parameters(), lr=CFG.lr, weight_decay=CFG.weight_decay, amsgrad=False)
+    scheduler = get_scheduler(optimizer)
+
+    # ====================================================
+    # apex
+    # ====================================================
+    if CFG.apex:
+        model, optimizer = amp.initialize(model, optimizer, opt_level='O1', verbosity=0)
+
+    # ====================================================
+    # loop
+    # ====================================================
+    criterion = TaylorCrossEntropyLoss(smoothing=CFG.smoothing)
+
+    best_score = 0.
+    best_loss = np.inf
+    
+    for epoch in range(CFG.epochs):
+        
+        start_time = time.time()
+        
+        # train
+        avg_loss = train_fn(train_loader, model, criterion, optimizer, epoch, scheduler, device)
+
+        # eval
+        avg_val_loss, preds = valid_fn(valid_loader, model, criterion, device)
+        valid_labels = valid_folds[CFG.target_col].values
+        
+        if isinstance(scheduler, ReduceLROnPlateau):
+            scheduler.step(avg_val_loss)
+        elif isinstance(scheduler, CosineAnnealingLR):
+            scheduler.step()
+        elif isinstance(scheduler, CosineAnnealingWarmRestarts):
+            scheduler.step()
+
+        # scoring
+        score = get_score(valid_labels, preds.argmax(1))
+
+        elapsed = time.time() - start_time
+
+        LOGGER.info(f'Epoch {epoch+1} - avg_train_loss: {avg_loss:.4f}  avg_val_loss: {avg_val_loss:.4f}  time: {elapsed:.0f}s')
+        LOGGER.info(f'Epoch {epoch+1} - Accuracy: {score}')
+
+        if score > best_score:
+            best_score = score
+            LOGGER.info(f'Epoch {epoch+1} - Save Best Score: {best_score:.4f} Model')
+            torch.save({'model': model.state_dict(), 
+                        'preds': preds},
+                        OUTPUT_DIR+f'MLP_fold{fold}_best.pth')
+    
+    check_point = torch.load(OUTPUT_DIR+f'MLP_fold{fold}_best.pth')
+    valid_folds[[str(c) for c in range(5)]] = check_point['preds']
+    valid_folds['preds'] = check_point['preds'].argmax(1)
+
+    return valid_folds
+def main():
+
+    def get_result(result_df):
+        preds = result_df['preds'].values
+        labels = result_df[CFG.target_col].values
+        score = get_score(labels, preds)
+        LOGGER.info(f'Score: {score:<.5f}')
+        
+    features = ['0_MODEL1','1_MODEL1','2_MODEL1','3_MODEL1','4_MODEL1',
+                '0_MODEL2','1_MODEL2','2_MODEL2','3_MODEL2','4_MODEL2',
+                '0_MODEL3','1_MODEL3','2_MODEL3','3_MODEL3','4_MODEL3']
+    
+    if CFG.train:
+        # train 
+        oof_df = pd.DataFrame()
+        for fold in range(CFG.n_fold):
+            if fold in CFG.trn_fold:
+                _oof_df = train_loop(oof, fold, features)
+                oof_df = pd.concat([oof_df, _oof_df])
+                LOGGER.info(f"========== fold: {fold} result ==========")
+                get_result(_oof_df)
+                
+        # CV result
+        LOGGER.info(f"========== CV ==========")
+        get_result(oof_df)
+        
+        # save result
+        oof_df.to_csv(OUTPUT_DIR+'oof_df.csv', index=False)
+    
+    if CFG.inference:      
+        # inference
+        model = MLP()
+        states = [torch.load(OUTPUT_DIR+f'MLP_fold{fold}_best.pth') for fold in CFG.trn_fold]
+        test_dataset = MLPDataset_test(test[features].values)
+        test_loader = DataLoader(test_dataset, batch_size=CFG.batch_size, shuffle=False, 
+                                 num_workers=CFG.num_workers, pin_memory=True)
+        predictions = inference(model, states, test_loader, device)
+        # submission
+        test['label'] = predictions.argmax(1)
+        test[['image_id', 'label']].to_csv(OUTPUT_DIR+'submission.csv', index=False)
